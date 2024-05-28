6c5ddd799e4448cf62151ec6e61cb03661e1f654
ZiadAlsaedi <ziad12399@gmail.com>
2024-05-28 18:15:50 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+ setInterval(function () {let url = 'https://coffee.alexflipnote.dev/random.json'
+ fetch(url , {method:"GET",})
+   .then((response) => response.json())
+ .then(data => {
+ image=data
+ let coffee =document.getElementById('img')
+ coffee.src=data.file
+ console.log(coffee);
+ coffee.append(coffee)
+  });
+ }, 5000); 
